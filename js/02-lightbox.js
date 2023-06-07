d8556e7b4ce55a7e0f1d871707c79b9d58c64e57
AnastasiiaKhranovska <123419111+AnastasiiaKhranovska@users.noreply.github.com>
2023-06-07 15:51:58 -0500

    dz

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const items = [];
+
+galleryItems.forEach(element => {
+const galleryItem = document.createElement('ul');
+galleryItem.className = 'gallery__item';
+
+const galleryLink = document.createElement ('a')
+galleryLink.className = 'gallery__link';
+galleryLink.href = element.original;
+
+const galleryImage = document.createElement('img');
+galleryImage.className = 'gallery__image';
+galleryImage.src = element.preview;
+galleryImage.setAttribute('title', element.description);
+galleryImage.alt = element.description;
+
+galleryLink.append(galleryImage);
+items.push(galleryLink);
+})
+gallery.append(...items);
+
+new SimpleLightbox('.gallery a',{
+    captionDelay: 250
+})
